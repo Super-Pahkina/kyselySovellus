51467cf99b8e26c74da7ac842cf2ccdd9d1f21cd
JoniMKarvinen <Joni.Karvinen@Hotmail.com>
2020-10-29 09:33:55 +0200

    Kysymys ja kysymys repo

--- src/main/java/superpähkinä/ohjelmistoprojekti/domain/Kysymys.java
+++ src/main/java/superpähkinä/ohjelmistoprojekti/domain/Kysymys.java
@@ -7,6 +7,75 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-public class Kysymys {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import superpähkinä.ohjelmistoprojekti.domain.Kysely;
+
+@Entity
+public class Kysymys {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long kysymys_id;
+	String tyyppi;
+	Boolean vaadittu;
+	String kysymys;
+	
+	@ManyToOne
+    @JsonIgnoreProperties ("kysymys") 
+    @JoinColumn(name = "kysely_id")
+    private Kysely kysely;
+
+	public Kysymys(String tyyppi, Boolean vaadittu, String kysymys, Kysely kysely) {
+		super();
+		this.tyyppi = tyyppi;
+		this.vaadittu = vaadittu;
+		this.kysymys = kysymys;
+		this.kysely = kysely;
+	}
+
+	public Long getKysymys_id() {
+		return kysymys_id;
+	}
+
+	public void setKysymys_id(Long kysymys_id) {
+		this.kysymys_id = kysymys_id;
+	}
+
+	public String getTyyppi() {
+		return tyyppi;
+	}
+
+	public void setTyyppi(String tyyppi) {
+		this.tyyppi = tyyppi;
+	}
+
+	public Boolean getVaadittu() {
+		return vaadittu;
+	}
+
+	public void setVaadittu(Boolean vaadittu) {
+		this.vaadittu = vaadittu;
+	}
+
+	public String getKysymys() {
+		return kysymys;
+	}
+
+	public void setKysymys(String kysymys) {
+		this.kysymys = kysymys;
+	}
+
+	public Kysely getKysely() {
+		return kysely;
+	}
+
+	public void setKysely(Kysely kysely) {
+		this.kysely = kysely;
+	}
+
+	@Override
+	public String toString() {
+		return "Kysymys [tyyppi=" + tyyppi + ", vaadittu=" + vaadittu + ", kysymys=" + kysymys + ", kysely=" + kysely + "]";
+	}
+	
 }
---
+++ src/main/java/superpähkinä/ohjelmistoprojekti/domain/KysymysRepository.java
@@ -0,0 +1,8 @@
+package superpähkinä.ohjelmistoprojekti.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface KysymysRepository extends CrudRepository<Kysymys, Long> {
+
+
+}
