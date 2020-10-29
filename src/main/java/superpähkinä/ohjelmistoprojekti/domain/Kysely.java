30ad851618ab17d27fd28c5c4b8789fc309a9a36
Kristiina Kumila <kristiinakumila@gmail.com>
2020-10-29 09:35:26 +0200

    kysely entity added

--- src/main/java/superpähkinä/ohjelmistoprojekti/domain/Kysely.java
+++ src/main/java/superpähkinä/ohjelmistoprojekti/domain/Kysely.java
@@ -1,5 +1,59 @@
 package superpähkinä.ohjelmistoprojekti.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
 public class Kysely {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long kysely_id;
+	private String nimi;
+	private LocalDate luontipvm;
+	
+	public Kysely() {
+		
+	}
+	
+	public Kysely(long kysely_id, String nimi) {
+
+		this.kysely_id = kysely_id;
+		this.nimi = nimi;
+		this.luontipvm = LocalDate.now();
+	}
+
+	public long getKysely_id() {
+		return kysely_id;
+	}
+
+	public void setKysely_id(long kysely_id) {
+		this.kysely_id = kysely_id;
+	}
+
+	public String getNimi() {
+		return nimi;
+	}
+
+	public void setNimi(String nimi) {
+		this.nimi = nimi;
+	}
+
+	public LocalDate getLuontipvm() {
+		return luontipvm;
+	}
+
+	public void setLuontipvm(LocalDate luontipvm) {
+		this.luontipvm = luontipvm;
+	}
+
+	@Override
+	public String toString() {
+		return "Kysely [kysely_id=" + kysely_id + ", nimi=" + nimi + ", luontipvm=" + luontipvm + "]";
+	}
+	
 
 }
