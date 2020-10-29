06dfe8ff165c76424700f4f15595935de1100dae
JoniMKarvinen <Joni.Karvinen@Hotmail.com>
2020-10-29 09:25:56 +0200

    dependency

--- src/main/java/superpähkinä/ohjelmistoprojekti/domain/Kysymys.java
+++ src/main/java/superpähkinä/ohjelmistoprojekti/domain/Kysymys.java
@@ -1,5 +1,12 @@
 package superpähkinä.ohjelmistoprojekti.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 public class Kysymys {
 
 }
