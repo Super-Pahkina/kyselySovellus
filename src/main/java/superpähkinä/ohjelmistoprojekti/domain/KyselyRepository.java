ef85848d54f844c61db410fde90c281850dbf8e3
Kristiina Kumila <kristiinakumila@gmail.com>
2020-10-29 10:29:13 +0200

    kyselyrep added

---
+++ src/main/java/superpähkinä/ohjelmistoprojekti/domain/KyselyRepository.java
@@ -0,0 +1,7 @@
+package superpähkinä.ohjelmistoprojekti.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface KyselyRepository extends CrudRepository<Kysely, Long>{
+
+}
--- src/main/java/superpähkinä/ohjelmistoprojekti/webcontroller/KyselyController.java
+++ src/main/java/superpähkinä/ohjelmistoprojekti/webcontroller/KyselyController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import superpähkinä.ohjelmistoprojekti.domain.Kysely;
+import superpähkinä.ohjelmistoprojekti.domain.KyselyRepository;
 import superpähkinä.ohjelmistoprojekti.domain.KysymysRepository;
 
 
