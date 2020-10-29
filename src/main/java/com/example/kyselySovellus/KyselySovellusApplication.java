1ec4ce966f5faf7f517474bbecb3bb46dd534e0d
Markus Karjalainen <markus.m.karjalainen@gmail.com>
2020-10-29 09:06:58 +0200

    initial commit

---
+++ src/main/java/com/example/kyselySovellus/KyselySovellusApplication.java
@@ -0,0 +1,13 @@
+package com.example.kyselySovellus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyselySovellusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyselySovellusApplication.class, args);
+	}
+
+}
