6e5d2467b723ffb16ce03dbc4b56ae9ceceeeea2
JoniMKarvinen <Joni.Karvinen@Hotmail.com>
2020-11-03 13:52:56 +0200

    .

--- src/main/java/com/example/kyselySovellus/KyselySovellusApplication.java
+++ src/main/java/com/example/kyselySovellus/KyselySovellusApplication.java
@@ -2,10 +2,17 @@ package com.example.kyselySovellus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KyselySovellusApplication {
-
+public class KyselySovellusApplication extends SpringBootServletInitializer {
+		
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(KyselySovellusApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(KyselySovellusApplication.class, args);
 	}
