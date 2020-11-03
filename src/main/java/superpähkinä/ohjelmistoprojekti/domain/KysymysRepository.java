bc2b75cf02a871642c865fe8d733d6bf63cbf23e
Markus Karjalainen <markus.m.karjalainen@gmail.com>
2020-11-03 14:56:38 +0200

    edit KysymysRepository.java

--- src/main/java/superpähkinä/ohjelmistoprojekti/domain/KysymysRepository.java
+++ src/main/java/superpähkinä/ohjelmistoprojekti/domain/KysymysRepository.java
@@ -1,7 +1,10 @@
 package superpähkinä.ohjelmistoprojekti.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface KysymysRepository extends CrudRepository<Kysymys, Long> {
 
+	List<Kysymys> findByName(String name);
 }
