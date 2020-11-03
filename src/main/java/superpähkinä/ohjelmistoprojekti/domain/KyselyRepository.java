ac22555ce614567f5c0439d93f8325fc50bfcbf9
Markus Karjalainen <markus.m.karjalainen@gmail.com>
2020-11-03 14:29:50 +0200

    edit

--- src/main/java/superpähkinä/ohjelmistoprojekti/domain/KyselyRepository.java
+++ src/main/java/superpähkinä/ohjelmistoprojekti/domain/KyselyRepository.java
@@ -1,7 +1,11 @@
 package superpähkinä.ohjelmistoprojekti.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 public interface KyselyRepository extends CrudRepository<Kysely, Long>{
 
+	List<Kysely> findByName(String nimi);
 }
